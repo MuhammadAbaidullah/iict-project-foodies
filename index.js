5088e07d3ba3f1fb0e67edb0d6a5095e7d78a5fe
MuhammadAbaidullah <99158647+MuhammadAbaidullah@users.noreply.github.com>
2022-02-10 14:39:36 -0800

    Final Lab project IICT - Foodies

---
+++ index.js
@@ -0,0 +1,40 @@
+function burgers(){
+
+document.getElementById('Zinger burger')
+document.getElementById('chicken burger')
+document.getElementById('Beef burger')
+}
+function BBQ(){
+
+document.getElementById('Reshmi Kebab')
+document.getElementById('Malai Boti')
+document.getElementById('Chicken Tikka')
+}
+function Pizza(){
+
+document.getElementById('Fajita Pizza')
+document.getElementById('Chicken Tikka Pizza')
+document.getElementById('Lasagnia Pizza')
+}
+function Sandwitch(){
+
+document.getElementById('Chicken Sandwitch')
+document.getElementById('Club Sandwitch')
+document.getElementById('Beef Sandwitch')
+}
+function Smoothie(){
+
+document.getElementById('Banana Smoothie')
+document.getElementById('Chocolate Smoothie')
+document.getElementById('Strawbarry Smoothie')
+}
+function Drinks(){
+
+document.getElementById('Sting')
+document.getElementById('Coke')
+document.getElementById('Pepsi')
+}
+function back(){
+
+document.getElementById('back')
+
